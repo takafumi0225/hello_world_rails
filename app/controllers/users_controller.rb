fcca7a9bc5d21f21458fe7b918d5304463c287ed
貴文 <takafumi0225s@gmail.com>
2019-10-15 14:27:10 +0900

    users_controller の CRUD 処理を実装しよう 〜Update, Delete〜

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,26 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :update, :destroy]
   def index
     @users = User.all
   end
 
   def show
+  end
+
+  def update
+    @user.update!(user_params)
+  end
+
+  def destroy
+    @user.destroy!
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:account, :name, :email)
+    end
+
+  def set_user
     @user = User.find(params[:id])
   end
 end
