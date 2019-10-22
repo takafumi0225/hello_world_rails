3105c060002c54a2aee99d3e632ee6fd320a4b25
貴文 <takafumi0225s@gmail.com>
2019-10-22 15:14:26 +0900

    FactoryBot でテストデータを作成しよう

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,14 +3,14 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   context "accountを指定しているとき" do
     it "ユーザーが作られる" do
-      user = User.new(name: "chinju", account: "chinju", email: "chinju@example.com")
+      user = create(:user)
       expect(user.valid?).to eq true
     end
   end
 
   context "accountが指定されていない場合" do
     it "エラーする" do
-      user = User.new(name: "chinju",account: nil,email: "chinju@example.com")
+      user = build(:user, account: nil)
       user.valid?
 
       expect(user.errors.messages[:account]).to include "can't be blank"
@@ -19,8 +19,8 @@ RSpec.describe User, type: :model do
 
   context "同名のaccountが存在するとき" do
     it "エラーする" do
-      User.create!(name: "chinju", account: "chinju", email: "chinju@example.com")
-      user = User.new(name: "chinju", account: "chinju", email: "chinju@example.com")
+      create(:user, account: "chinju")
+      user = build(:user, account: "chinju")
       user.valid?
 
       expect(user.errors.messages[:account]).to include "has already been taken"
